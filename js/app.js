80a3d82e63da7f839b6eeca5ffe1b4a8b5933184
ksion4ik1 <ksion4ik.life@gmail.com>
2020-01-25 03:16:15 +0200

    Criate index and add more info in ModelS

--- js/app.js
+++ js/app.js
@@ -41,4 +41,3 @@ $('#slider-news').slick({
       });
     }
   }
-  
---
+++ js/appgen.js
@@ -0,0 +1,23 @@
+  // -------------------------For ScrollArrow-------------------------------------------
+  let allArrow = document.querySelectorAll(".arrowGen");
+  for (let i = 0; i < allArrow.length; i++) {
+    allArrow[i].onclick = function (event) {
+      event.preventDefault();
+      let blockID = this.getAttribute('href');
+      document.querySelector('' + blockID).scrollIntoView({
+        behavior: 'smooth',
+        block: 'start',
+      });
+    }
+  }
+
+  for (let i = 0; i < allArrow.length; i++) {
+    allArrow[i].ontouchstart = function (event) {
+      event.preventDefault();
+      let blockID = this.getAttribute('href');
+      document.querySelector('' + blockID).scrollIntoView({
+        behavior: 'smooth',
+        block: 'start',
+      });
+    }
+  }
